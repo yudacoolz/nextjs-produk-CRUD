9afd90329f6eded70ffdfc81ca280b537a1bf593
TJOK YUDA <tjokyuda98@gmail.com>
2024-10-30 19:35:52 +0700

    coba lagi localstorage

--- src/contexts/useAuth.tsx
+++ src/contexts/useAuth.tsx
@@ -35,37 +35,52 @@ import { useEffect, useState } from "react";
 // import axios from "axios";
 
 export const useAuth = () => {
-  // const [isAuthenticated, setIsAuthenticated] = useState<boolean | null>(null);
-  const [isAuthenticated, setIsAuthenticated] = useState(
-    localStorage.getItem("token") ? true : false
-  );
+  const [isToken, setIsToken] = useState<String | null>(null);
+  const [isAuthenticated, setIsAuthenticated] = useState<Boolean | null>(null);
+  // const [isAuthenticated, setIsAuthenticated] = useState(
+  //   localStorage.getItem("token") ? true : false
+  // );
+
+  // useEffect(() => {
+  //   const checkAuth = async () => {
+  //     // const token = localStorage.getItem("token");
+  //     if (isAuthenticated) {
+  //       // try {
+  //       //   const { data } = await axios.get("/api/auth", {
+  //       //     headers: { Authorization: `Bearer ${token}` },
+  //       //   });
+
+  //       //   setIsAuthenticated(!!data.userId);
+  //       // } catch {
+  //       //   setIsAuthenticated(false);
+  //       // }
+
+  //       setIsAuthenticated(true);
+
+  //       // const token = localStorage.getItem("token");
+  //       // setIsAuthenticated(!!token);
+  //     } else {
+  //       setIsAuthenticated(false);
+  //     }
+  //   };
+  //   if (typeof window !== "undefined") {
+  //     checkAuth();
+  //   }
+  // }, []);
 
   useEffect(() => {
-    const checkAuth = async () => {
-      // const token = localStorage.getItem("token");
-      if (isAuthenticated) {
-        // try {
-        //   const { data } = await axios.get("/api/auth", {
-        //     headers: { Authorization: `Bearer ${token}` },
-        //   });
-
-        //   setIsAuthenticated(!!data.userId);
-        // } catch {
-        //   setIsAuthenticated(false);
-        // }
-
-        setIsAuthenticated(true);
-
-        // const token = localStorage.getItem("token");
-        // setIsAuthenticated(!!token);
-      } else {
-        setIsAuthenticated(false);
-      }
-    };
-    if (typeof window !== "undefined") {
-      checkAuth();
-    }
+    const savedValue = window.localStorage.getItem("token");
+    setIsToken(savedValue ? savedValue : "");
   }, []);
+
+  useEffect(() => {
+    if (isToken) {
+      setIsAuthenticated(true);
+      // if (typeof isToken === "string") {
+      //   window.localStorage.setItem("isAuth", "true");
+      // }
+    }
+  }, [isToken]);
 
   return isAuthenticated;
 };
