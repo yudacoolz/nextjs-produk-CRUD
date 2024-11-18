4b7cccc79fadcb76d4b45178a804d84e3eff6b42
TJOK YUDA <tjokyuda98@gmail.com>
2024-11-18 23:06:12 +0700

    updatee

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import axios from "axios";
-import { useProductContext } from "@/contexts/ProductContext";
+// import { useProductContext } from "@/contexts/ProductContext";
 
 interface ProductData {
   id: number;
